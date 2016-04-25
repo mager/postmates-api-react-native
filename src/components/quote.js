3d83381171c2a79f434c0036580dc56476aab7ce
Andrew Mager <mager@postmates.com>
2016-04-25 15:21:55 -0700

    Reformat and display the response

--- src/components/quote.js
+++ src/components/quote.js
@@ -15,81 +15,84 @@ const URL = 'https://api.postmates.com/v1/customers';
 
 
 module.exports = React.createClass({
-  getInitialState: function() {
-    return {
-        pickup_address: '',
-        dropoff_address: ''
-    };
-  },
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text>Get a delivery quote</Text>
+    getInitialState: function() {
+        return {
+            pickup_address: '100 Market St, San Francisco, CA',
+            dropoff_address: '425 Market St, San Francisco, CA',
+            quote_price: ''
+        };
+    },
+    render: function() {
+        return (
+            <View style={styles.container}>
+            <Text>Get a delivery quote</Text>
 
-        <Text style={styles.label}>Pickup address:</Text>
-        <TextInput
-          style={styles.input}
-          value={this.state.pickup_address}
-          onChangeText={(text) => this.setState({
-            pickup_address: text
-          })} />
+            <Text style={styles.label}>Pickup address:</Text>
+            <TextInput
+                style={styles.input}
+                value={this.state.pickup_address}
+                onChangeText={(text) => this.setState({
+                pickup_address: text
+            })} />
 
-        <Text style={styles.label}>Dropoff address:</Text>
-        <TextInput
-          style={styles.input}
-          value={this.state.dropoff_address}
-          onChangeText={(text) => this.setState({
-            dropoff_address: text
-          })} />
+            <Text style={styles.label}>Dropoff address:</Text>
+            <TextInput
+                style={styles.input}
+                value={this.state.dropoff_address}
+                onChangeText={(text) => this.setState({
+                dropoff_address: text
+            })} />
 
-        <Text style={styles.label}>{this.state.errorMessage}</Text>
-        <Button text={'Get Quote'} onPress={this.onPress} />
-      </View>
-    );
-  },
-  onPress: function() {
-    const REQUEST_URL = `${URL}/${keys.customer_key}/delivery_quotes`;
+            <Text style={styles.label}>{this.state.errorMessage}</Text>
+            <Text style={styles.label}>{this.state.quote_price}</Text>
+            <Button text={'Get Quote'} onPress={this.onPress} />
+            </View>
+        );
+    },
+    onPress: function() {
+        const REQUEST_URL = `${URL}/${keys.customer_key}/delivery_quotes`;
+        var data = querystring.stringify({
+            pickup_address: this.state.pickup_address,
+            dropoff_address: this.state.dropoff_address
+        });
 
-    var data = querystring.stringify({
-      pickup_address: '100 Market St, San Francisco, CA',
-      dropoff_address: '425 Market St, San Francisco, CA'
-    });
-
-    axios({
-        method: 'post',
-        url: REQUEST_URL,
-        data: data,
-        headers: {
-          'Authorization': keys.api_key,
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
-      })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (response) {
-        console.log(response);
-      });
-  }
+        axios({
+            method: 'post',
+            url: REQUEST_URL,
+            data: data,
+            headers: {
+                'Authorization': keys.api_key,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        })
+        .then((response) => {
+            this.setState({
+                quote_price: response.data.fee
+            });
+        })
+        .catch((response) => {
+            console.log(response);
+        });
+    }
 });
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  input: {
-    padding: 4,
-    height: 40,
-    borderColor: '#ddd',
-    borderWidth: 1,
-    borderRadius: 3,
-    margin: 5,
-    width: 200,
-    alignSelf: 'center'
-  },
-  label: {
-    fontSize: 18
-  }
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    input: {
+        padding: 4,
+        height: 40,
+        borderColor: '#ddd',
+        borderWidth: 1,
+        borderRadius: 3,
+        margin: 5,
+        width: 200,
+        alignSelf: 'center'
+    },
+    label: {
+        fontSize: 18
+    }
 });
--- src/main.js
+++ src/main.js
@@ -7,30 +7,30 @@ var {
 var Quote = require('./components/quote');
 
 var ROUTES = {
-  quote: Quote
+    quote: Quote
 }
 
 
 module.exports = React.createClass({
-  renderScene: function(route, navigator) {
-    var Component = ROUTES[route.name]; // ROUTES['signin'] => SignIn
-    return <Component route={route} navigator={navigator} />;
-  },
-  render: function() {
-    return (
-      <Navigator
-        style={styles.container}
-        initialRoute={{name: 'quote'}}
-        renderScene={this.renderScene}
-        configureScene={() => {
-          return Navigator.SceneConfigs.FloatFromRight;
-        }} />
-    );
-  }
+    renderScene: function(route, navigator) {
+        var Component = ROUTES[route.name]; // ROUTES['signin'] => SignIn
+        return <Component route={route} navigator={navigator} />;
+    },
+    render: function() {
+        return (
+            <Navigator
+            style={styles.container}
+            initialRoute={{name: 'quote'}}
+            renderScene={this.renderScene}
+            configureScene={() => {
+                return Navigator.SceneConfigs.FloatFromRight;
+            }} />
+        );
+    }
 });
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1
-  }
+    container: {
+        flex: 1
+    }
 });
